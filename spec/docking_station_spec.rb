a4f4f7e5334f7ff7cef4263393d14fce8b79ec13
Joshua Brook <joshuabrookuk@gmail.com>
2020-08-29 08:09:18 +0100

    REFCTOR: DockStat respond release_bike. Used one-liner syntax

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,5 @@
 require 'docking_station'
 
 describe DockingStation do
-  it 'should respond to release_bike' do
-    expect(subject).to respond_to :release_bike
+    it { should respond_to :release_bike }
   end
-end
