b3d46cc6894cf73796de82e49b2416e98f134663
Joshua Brook <joshuabrookuk@gmail.com>
2020-09-02 14:45:02 +0100

    RED: Bike.working? should be true (test typo).

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,6 +4,6 @@ describe Bike do
   it { should respond_to :working? }
 
   it "should be a working bike" do
-    expect (subject.working?).to be_true
+    expect(subject.working?).to be true
   end
 end
