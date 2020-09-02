29247aa8fda6731b51ebf2dc9e9290cfcbfdc5b3
Joshua Brook <joshuabrookuk@gmail.com>
2020-09-02 14:19:43 +0100

    RED: DockingStation should release Bike class.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,8 @@ require 'docking_station'
 
 describe DockingStation do
   it { should respond_to :release_bike }
+
+  it "should release a bike" do
+    expect (subject.release_bike).should eq (Bike.new)
+  end
 end
