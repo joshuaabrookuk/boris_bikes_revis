ccca04a0f14157136d1fe915e500f4d55f641012
Joshua Brook <joshuabrookuk@gmail.com>
2020-09-05 07:24:25 +0100

    REFACTOR: DockingStation using OOP and private methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -6,13 +6,23 @@ class DockingStation
   end
 
   def release_bike
-    raise "No bike available!" if @bikes.empty?
+    raise "No bike available!" if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    raise "DockingStation full!" if @bikes.count >= 20
+    raise "DockingStation full!" if full?
     @bikes << bike
   end
-  
+
+  private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
+  end
+
 end
