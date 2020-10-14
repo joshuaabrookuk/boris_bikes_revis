abe3c2475b42cd5e5515516217a275dd8d807964
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-10-14 15:50:53 +0100

    REFACTOR: Changing a parameter name and cleaning test syntax and mocking.

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -3,16 +3,17 @@
 require 'garage'
 
 describe Garage do
+  let(:bike) { double :bike }
 
   it { should respond_to(:fix) }
 
   describe '#fix' do
     it 'should fix all bikes in the garage' do
-      bike = Bike.new
-      bike.condition(false)
+      allow(bike).to receive(:condition).with(false).and_return(false)
+      allow(bike).to receive(:status).and_return(true)
       subject.bikes << bike
       subject.fix
-      expect(subject.bikes[0].status).to eq (true)
+      expect(subject.bikes[0].status).to eq true
     end
   end
 end
