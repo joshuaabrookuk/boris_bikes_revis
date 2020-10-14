d1e36fd5687fd781e559cb8e7db63ebf9a8d03cd
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-10-14 16:09:00 +0100

    REFACTOR: Further refactor to fix broken test.

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -4,15 +4,20 @@ require 'garage'
 
 describe Garage do
   let(:bike) { double :bike }
+  let(:van) { double :van }
+  let(:dockingstation) { double :docking_station }
 
   it { should respond_to(:fix) }
 
   describe '#fix' do
     it 'should fix all bikes in the garage' do
       allow(bike).to receive(:condition).with(false).and_return(false)
+      allow(dockingstation).to receive(:dock).with(bike).and_return(bike)
+      allow(van).to receive(:collect_bikes).with(dockingstation).and_return(bike)
+      allow(van).to receive(:drop_off_bikes).with(subject).and_return(bike)
+      allow(subject).to receive(:bikes).and_return([bike])
+      allow(subject).to receive(:fix).and_return([bike])
       allow(bike).to receive(:status).and_return(true)
-      subject.bikes << bike
-      subject.fix
       expect(subject.bikes[0].status).to eq true
     end
   end
